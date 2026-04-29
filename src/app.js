5414e2b8446f4780393b58329ebfdde93a1fae51
marpaz90-lgtm <marpaz90@gmail.com>
2026-04-29 13:10:40 +0000

    generador de cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦","♥","♠","♣"];
+  let suitClass = ["diamante","corazones","espadas","treboles"];
+  let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+  let randomSuit = Math.floor(Math.random()*suit.length);
+  let randomNumber = Math.floor(Math.random()*numbers.length);
+  let suitUp = document.getElementById("suit-up");
+  let suitDown = document.getElementById("suit-down");
+  let number = document.getElementById("number");
+  let card = document.getElementById("card");
+
+  suitUp.innerHTML = suit[randomSuit];
+  suitDown.innerHTML = suit[randomSuit];
+  number.innerHTML = numbers[randomNumber];
+
 };
